154b012ce811d36d7ddde03ef4686ca651d909b2
Nicolas <nicolasalmeida321@gmail.com>
2025-02-16 16:59:05 -0300

    Add validations to 'Places'

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,5 @@
 class Place < ApplicationRecord
+  validates :name, :longitude, :latitude, presence: true
+  validates :latitude,  numericality: { greater_than_or_equal_to: -90,  less_than_or_equal_to: 90 }
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 end
